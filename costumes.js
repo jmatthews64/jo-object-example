32fcc2cc43469abf03edff155001d3634375f124
jmatthews64 <jon.matthews64@gmail.com>
2023-10-16 14:47:36 -0400

    Created files

---
+++ costumes.js
@@ -0,0 +1,25 @@
+//stores data about costumes in JSON format
+// JSON is JavaScript Object Notation
+
+costumes = [
+    {
+        //this is called a key, value pair
+        'charater': 'Dragon', 
+        'number of pieces': 5,
+        "photo_path": "images/dragon_costume.png",
+        "actor": "Keanu Reeves",
+        "cleaned": true,
+    },
+    {
+        'charater': 'Donkey', 
+        'number of pieces': 4,
+        "photo_path": "https://www.amazon.com/Fun-Costumes-Adult-Donkey-Costume/dp/B089B5R4SD",
+        "actor": "Jamie Foxx",
+        "cleaned": false, 
+    } 
+]
+
+//We can retrieve valuse from objects with dot notation
+for (let costume of costumes) {
+    console.log(costume.charater);
+}
