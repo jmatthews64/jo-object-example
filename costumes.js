3b8468e8bb5ce71a30690c7f2dd2737337313e4b
jmatthews64 <jon.matthews64@gmail.com>
2023-10-16 15:14:24 -0400

    completed task 1 to create an array of our team with info

--- costumes.js
+++ costumes.js
@@ -1,26 +1,59 @@
-//stores data about costumes in JSON format
-// JSON is JavaScript Object Notation
+// //stores data about costumes in JSON format
+// // JSON is JavaScript Object Notation
 
-costumes = [
+// costumes = [
+//     {
+//         //this is called a key, value pair
+//         'charater': 'Dragon', 
+//         'number of pieces': 5,
+//         "photo_path": "images/dragon_costume.png",
+//         "actor": "Keanu Reeves",
+//         "cleaned": true,
+//     },
+//     {
+//         'charater': 'Donkey', 
+//         'number of pieces': 4,
+//         "photo_path": "https://www.amazon.com/Fun-Costumes-Adult-Donkey-Costume/dp/B089B5R4SD",
+//         "actor": "Jamie Foxx",
+//         "cleaned": false, 
+//     } 
+// ]
+
+// //We can retrieve valuse from objects with dot notation
+// for (let costume of costumes) {
+//     console.log(costume.charater);
+//     console.log(`It has ${costume['number of pieces']} pieces.`)
+// }
+
+team = [
     {
-        //this is called a key, value pair
-        'charater': 'Dragon', 
-        'number of pieces': 5,
-        "photo_path": "images/dragon_costume.png",
-        "actor": "Keanu Reeves",
-        "cleaned": true,
+        "name": 'Maria',
+        "city": "Raleigh",
+        "food": "Doughnuts",
+        "live show": "Colbert Report",
     },
     {
-        'charater': 'Donkey', 
-        'number of pieces': 4,
-        "photo_path": "https://www.amazon.com/Fun-Costumes-Adult-Donkey-Costume/dp/B089B5R4SD",
-        "actor": "Jamie Foxx",
-        "cleaned": false, 
-    } 
+        "name": 'Freddie',
+        "city": "Hillsborough",
+        "food": "baked mac and cheese",
+        "live show": "Shania Twain",
+    },
+    {
+        "name": 'Jon',
+        "city": "Durham",
+        "food": "tacos",
+        "live show": "Volunteer Jam",
+    },
+    {
+        "name": 'Jose',
+        "city": 'Durham',
+        "food": "pizza",
+        "live show": "Disney on Ice",
+    },
+    {
+        "name": 'Kelsey',
+        "city": "Durham",
+        "food": "sandwiches",
+        "live show": "Phantom of the Opera",
+    }
 ]
-
-//We can retrieve valuse from objects with dot notation
-for (let costume of costumes) {
-    console.log(costume.charater);
-    console.log(`It has ${costume['number of pieces']} pieces.`)
-}
